984babd770fe42b242c6bdb136a30b3eea5c517c
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-09-17 20:01:55 +0530

    Time: 2 ms (70.38%), Space: 41.5 MB (21.22%) - LeetHub

--- 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
+++ 1693-sum-of-all-odd-length-subarrays/1693-sum-of-all-odd-length-subarrays.java
@@ -1,17 +1,15 @@
 class Solution {
     public int sumOddLengthSubarrays(int[] arr) {
-        int sum =0;
+        int all = 0;
         for(int i =0; i<arr.length; i++){
-            
+            int sum =0;
             for(int j=i; j<arr.length; j++){
-                
+                sum+=arr[j];
                 if((j-i + 1) % 2 == 1){
-                    for(int k =i; k<=j;k++){
-                        sum+=arr[k];
-                    }
+                    all += sum;
                 }
             }
         }
-        return sum;
+        return all;
     }
 }
