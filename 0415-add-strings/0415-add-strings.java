e0975d222e04934017d6e3919c2f0f66dea5ca3a
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-10-03 15:56:25 +0530

    Time: 1 ms (100%), Space: 42.8 MB (42.92%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,33 +1,19 @@
 class Solution {
     public String addStrings(String num1, String num2) {
-        int n1 = num1.length() - 1;
-        int n2 = num2.length() - 1;
-        int c =0;
-        StringBuilder str = new StringBuilder();
-        while(n1 >= 0 && n2 >=0 ){
-            int num = ((num1.charAt(n1) - '0') + (num2.charAt(n2) - '0'))+ c;
-            str.append(num%10);
-            c=num/10;
+        StringBuilder s = new StringBuilder();
+        int n1 = num1.length()-1;
+        int n2 = num2.length()-1;
+        int carry =0;
+        while(n1 >=0 || n2 >=0){
+            int res =( n1>=0?num1.charAt(n1)-'0': 0) + (n2 >= 0? num2.charAt(n2) - '0':0) + carry;
+            s.append(res%10);
+            carry = res/10;
             n1--;
             n2--;
         }
-       
-        while(n1>=0){
-            int num = (num1.charAt(n1) - '0') + c;
-            str.append(num%10);
-            c = num/10;
-            n1--;
+        if(carry == 1 && n1 <0 && n2 < 0){
+            s.append("1");
         }
-        while(n2>=0){
-            int num = (num2.charAt(n2) - '0') + c;
-            str.append(num%10);
-            c = num/10;
-            n2--;
-        }
-         if(c == 1 && n1 < 0 && n2 < 0){
-            str.append(1);
-        }
-        str.reverse();
-        return str.toString();
+        return s.reverse().toString();
     }
 }
