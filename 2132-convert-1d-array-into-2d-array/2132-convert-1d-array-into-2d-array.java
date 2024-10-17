e829b07cbe8710c6c20998b4ffc4c4918b95b303
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-10-18 01:47:12 +0530

    Time: 6 ms (8.66%), Space: 55.4 MB (58.53%) - LeetHub

--- 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
+++ 2132-convert-1d-array-into-2d-array/2132-convert-1d-array-into-2d-array.java
@@ -5,10 +5,14 @@ class Solution {
             return new int[0][0];
         }
         int idx =0;
-        for(int row = 0; row<m;row++){
+        /*for(int row = 0; row<m;row++){
             for(int col =0; col<n; col++){
                 res[row][col]= original[idx++];
             }
+        }*/ 
+        int col =0;
+        for(int i =0; i<original.length;i++){
+            res[i/n][i%n ]=original[i];
         }
         return res;
     }
