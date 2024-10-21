83fed2f1c1a7884986043ca27c96f8189af374f3
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-10-21 16:57:42 +0530

    Time: 1 ms (78.56%), Space: 45.6 MB (49.7%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.java
@@ -2,21 +2,33 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         int m=matrix.length;
         int n = matrix[0].length;
-        boolean[] row = new boolean[m];
-        boolean[] col = new boolean[n];
+        boolean firstrow = false;
+        boolean firstcol = false;
         for(int i =0 ;i< m; i++){
             for(int j=0; j<n; j++){
                 if(matrix[i][j] == 0){
-                    row[i]=true;
-                    col[j]=true;
+                    matrix[0][j]=0;
+                    matrix[i][0]=0;
+                    if(i == 0) firstrow = true;
+                    if(j == 0) firstcol = true;
                 }
             }
         }
-        for(int i =0; i<m;i++){
-            for(int j=0; j<n ;j++){
-                if(row[i]==true || col[j]==true){
+        for(int i =1; i<m;i++){
+            for(int j=1; j<n ;j++){
+                if(matrix[i][0]==0 || matrix[0][j]==0){
                     matrix[i][j]=0;
                 }
+            }
+        }
+        if(firstrow == true){
+            for(int i =0; i<matrix[0].length;i++){
+                matrix[0][i]=0;
+            }
+        }
+        if(firstcol == true){
+            for(int i =0; i<matrix.length;i++){
+                matrix[i][0]=0;
             }
         }
     }
