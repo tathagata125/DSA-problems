0e14ad5b1f13129f81f0c9f1ac580744c0a2c7d5
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-11-14 23:51:16 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Print Elements of Array/print-elements-of-array.java
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    // Driver code
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine().trim());
+        while (t-- > 0) {
+            String[] str = br.readLine().trim().split(" ");
+            int n = str.length;
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(str[i]);
+            }
+
+            new Solution().printArray(arr);
+            System.out.println();
+        
+System.out.println("~");
+}
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    // Just print the space seperated array elements
+    void print(int arr[], int n){
+        if(n== arr.length){
+            return;
+        }
+        System.out.print(arr[n]+" ");
+        print(arr,n+1);
+    }
+    void printArray(int arr[]) {
+        print( arr, 0);
+    }
+}
