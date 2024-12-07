88432e3abaec2e4b42df0022f3fde1d339521ea7
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-12-07 21:39:48 +0530

    Time: 0 ms (100%), Space: 43.1 MB (23.51%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int l =0;
+        int h = nums.length - 1;
+        while(l <= h){
+            int mid = l + (h - l)/2 ;
+            if(nums[mid] >= target){
+                h = mid - 1;
+            }
+            else if(nums[mid] < target){
+                l = mid +1;
+            }
+        }
+        return l;
+    }
+}
