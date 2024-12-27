cd06a9ff3cce3c24933e32a406b46d4b7b16e3da
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-12-27 19:07:28 +0530

    Time: 0 ms (100%), Space: 44.8 MB (13.13%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,27 +1,32 @@
 class Solution {
     List<List<Integer>> com;
-    void getAllCom(int[] nums,List<Integer>output,boolean[] vis,int idx){
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    void getAllCom(int[] nums,int idx){
         if(idx == nums.length){
-            com.add(new ArrayList<>(output));
+            List<Integer>output = new ArrayList<>();
+            for(int val: nums){
+                output.add(val);
+            }
+            com.add(output);
             return;
         }
         
-        for(int i = 0; i<nums.length;i++){
-            if(vis[i] == true){
-                continue;
-            }
-            output.add(nums[i]);
-            vis[i] = true;
-            getAllCom( nums, output,vis,idx+1);
-            output.remove(output.size()-1);
-            vis[i] = false;
+        for(int j = idx; j<nums.length;j++){
+            
+            swap(nums,idx,j);
+           
+            getAllCom( nums,idx+1);
+            
+            swap(nums,idx,j);
         }
     }
     public List<List<Integer>> permute(int[] nums) {
         com = new ArrayList<>();
-        boolean[] vis = new boolean[nums.length];
-        List<Integer>output = new ArrayList<>();
-        getAllCom(nums,output,vis,0);
+        getAllCom(nums,0);
         return com;
     }
 }
