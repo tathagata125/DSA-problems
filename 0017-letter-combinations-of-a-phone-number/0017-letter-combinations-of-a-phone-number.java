7fc823350446fff4e392108fb5a9df99786f80fe
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2024-12-28 19:45:34 +0530

    Time: 4 ms (33.26%), Space: 42.8 MB (6.4%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    List<String>ways;
+    String[] s = {"","","abc","def","ghi","jkl","mno","pqrs","tuv","wxyz"};
+    void paths(String digit, int idx, String output){
+        if(idx == digit.length()){
+            ways.add(output);
+            return;
+        }
+        int su = digit.charAt(idx) - '0';
+        for(char letter: s[su].toCharArray()){
+            paths(digit,idx+1,output+letter);
+        }
+    }
+    public List<String> letterCombinations(String digits) {
+      ways = new ArrayList<>();
+      if(digits.length() == 0) return ways;
+      paths(digits,0,"");
+      return ways;  
+    }
+}
