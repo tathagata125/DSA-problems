c5af55a1fa3f9dd514ec795526ec3dc93924e63b
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-01-26 18:14:51 +0530

    Time: 43 ms (22.21%), Space: 55.5 MB (18.53%) - LeetHub

--- 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
+++ 1014-k-closest-points-to-origin/1014-k-closest-points-to-origin.java
@@ -1,45 +1,41 @@
 class Solution {
-    class Node implements Comparable<Node>{
-        int start ;
-        int end;
-        public Node(int start, int end){
-            this.start = start;
-            this.end = end;
-        }
-        public int compareTo(Node other){
-            if(Math.sqrt(this.start*this.start + this.end*this.end) < Math.sqrt(other.start*other.start + other.end*other.end))return 1;
-            else if(Math.sqrt(this.start*this.start + this.end*this.end) > Math.sqrt(other.start*other.start + other.end*other.end))return -1;
-            else return 0;
-        }
-    }
+    
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<Node> pq = new PriorityQueue<>();
-        
-        for(int i = 0; i < points.length ; i++){
-            Node top=null ;
-            int s=0 ;
-            int e=0 ;
+        PriorityQueue<int[]>pq = new PriorityQueue<>(
+            (t,o)->{
+                double p1 = Math.sqrt(t[0]*t[0] + t[1]*t[1]);
+                double p2 = Math.sqrt(o[0]*o[0] + o[1]*o[1]);
+                if(p1 > p2)return -1;
+                else if(p1 < p2) return 1;
+                else return 0;
+            }
+        );
+        for(int[] pair: points){
+            int[] p = new int[2];
+            int s=0;
+            int e = 0;
+            double d=0.0 ;
             if(pq.size() != 0){
-                top = pq.peek();
-                s = top.start;
-                 e = top.end;
+                 p = pq.peek();
+                 s=p[0];
+                 e=p[1];
+                 d = Math.sqrt(s*s + e*e);
             }
             if(pq.size() < k){
-                pq.add(new Node(points[i][0],points[i][1]));
-            }else if(Math.sqrt(s*s + e*e) > Math.sqrt(points[i][0]*points[i][0] + points[i][1]*points[i][1]) ){
+                pq.add(pair);
+            }else if(Math.sqrt(pair[0]*pair[0] + pair[1]*pair[1]) < d){
                 pq.remove();
-                pq.add(new Node(points[i][0],points[i][1]));
+                pq.add(pair);
             }
-            
         }
         int[][] arr = new int[k][2];
-        for(int i = k-1 ; i>=0 ; i--){
-            Node top = pq.remove();
-            int s = top.start;
-            int e = top.end;
-            arr[i][0] = s;
-            arr[i][1] = e;
+        for(int i = k-1; i>=0; i--){
+            int[] p = pq.remove();
+            arr[i][0] = p[0];
+            arr[i][1] = p[1];
         }
         return arr;
+        
+        
     }
 }
