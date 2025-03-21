e61adba0bfe9ac0bc14c0033b751fc370257354a
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-03-21 08:40:52 +0530

    Time: 1 ms (82.98%), Space: 41.9 MB (45.11%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,5 +1,26 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        
+        char[] c1 = word1.toCharArray();
+        char[] c2 = word2.toCharArray();
+        StringBuilder s = new StringBuilder();
+        int s1 = 0;
+        int s2 = 0;
+        int i = 0;
+        while(s1 < c1.length && s2 < c2.length){
+            if( i%2 == 0){
+                s.append(c1[s1++]);
+            }
+            else{
+                s.append(c2[s2++]);
+            }
+            i++;
+        }
+        while(s1 < c1.length){
+            s.append(c1[s1++]);
+        }
+        while(s2 < c2.length){
+            s.append(c2[s2++]);
+        }
+        return s.toString();
     }
 }
