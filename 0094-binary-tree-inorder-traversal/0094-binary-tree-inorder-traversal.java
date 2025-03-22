50f630dd174202af527b23f0d182cddc4822fb12
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-03-22 15:16:13 +0530

    Time: 1 ms (2.23%), Space: 42 MB (15.61%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -14,16 +14,36 @@
  * }
  */
 class Solution {
-    List<Integer> node ;
-    void Traversal(TreeNode root){
-        if(root == null)return;
-        if(root.left != null)Traversal(root.left);
-        node.add(root.val);
-        if(root.right != null)Traversal(root.right);
+    class Pair{
+        int state = 1;
+        TreeNode root;
+        Pair(TreeNode root){
+            this.root = root;
+        }
     }
     public List<Integer> inorderTraversal(TreeNode root) {
-        node = new ArrayList<>();
-        Traversal(root);
-        return node;
+        List<Integer> inorder = new ArrayList<>();
+        Stack<Pair> st = new Stack<>();
+        if(root != null)st.push(new Pair(root));
+        while(st.size() > 0){
+            Pair node = st.peek();
+            if(node.state ==1){
+                st.peek().state++;
+                if(node.root.left != null){
+                    st.push(new Pair(node.root.left));
+                }
+            }
+            else if(node.state == 2){
+                inorder.add(node.root.val);
+                st.peek().state++;
+                if(node.root.right != null){
+                    st.push(new Pair(node.root.right));
+                }
+            }
+            else{
+                st.pop();
+            }
+        }
+        return inorder;
     }
 }
