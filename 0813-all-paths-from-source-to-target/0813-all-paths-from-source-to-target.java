66320fae9edc02ef66f4cf17691389f2137f2367
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-03-25 18:25:42 +0530

    Time: 1 ms (100%), Space: 46.3 MB (79.67%) - LeetHub

--- 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
+++ 0813-all-paths-from-source-to-target/0813-all-paths-from-source-to-target.java
@@ -1,43 +1,26 @@
 class Solution {
-    class Graph{
-        ArrayList<Integer>[] adj;
-        int n;
-        Graph(int n){
-            adj = new ArrayList[n];
-            this.n = n;
-            for(int i = 0; i < n; i++){
-                adj[i]=new ArrayList<>();
-            }
-        }
-        void addEdge(int src , int dest){
-            adj[src].add(dest);
-        }
-        List<List<Integer>> res = new ArrayList<>();
-        void hasPath(int src, List<Integer> ans){
-            if(src == n-1){
-                ans.add(n-1);
+    
+        List<List<Integer>> res;
+        void hasPath(int src,int[][] graph, ArrayList<Integer> ans){
+            if(src == graph.length - 1){
+                ans.add(graph.length-1);
                 res.add(new ArrayList<>(ans));
                 ans.remove(ans.size()-1);
                 return;
             }
             ans.add(src);
-            for(int m : adj[src]){
-                hasPath(m,ans);
+            for(int m : graph[src]){
+                hasPath(m,graph,ans);
             }
             ans.remove(ans.size()-1);
         }
 
-    }
+    
     
     public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
-         Graph g = new Graph(graph.length);
-        for(int i = 0; i < graph.length; i++){
-            for(int j = 0; j < graph[i].length; j++){
-                g.addEdge(i,graph[i][j]);
-            }
-        }
-        List<Integer> l = new ArrayList<>();
-         g.hasPath(0,l);
-         return g.res;
+        
+         res= new ArrayList<>();
+         hasPath(0,graph, new ArrayList<>());
+         return res;
     }
 }
