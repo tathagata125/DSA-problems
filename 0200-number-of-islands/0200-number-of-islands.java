0cb069cce3fe7e9e663d1c3619e075358fa27543
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-03-28 16:31:59 +0530

    Time: 4 ms (50.51%), Space: 49.8 MB (39.31%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,23 +1,23 @@
 class Solution {
-    void dfs(int i, int j,char[][]grid, int[][]vis){
+    void dfs(int i, int j,char[][]grid){
         if(i < 0 || i==grid.length || j<0 || j==grid[0].length)return;
         if(grid[i][j]=='0')return;
-        if(vis[i][j] == 1)return;
+        if(grid[i][j] == '2')return;
         
-        vis[i][j]=1;
-        dfs(i+1,j,grid,vis);
-        dfs(i-1,j,grid,vis);
-        dfs(i,j-1,grid,vis);
-        dfs(i,j+1,grid,vis);
+        grid[i][j]='2';
+        dfs(i+1,j,grid);
+        dfs(i-1,j,grid);
+        dfs(i,j-1,grid);
+        dfs(i,j+1,grid);
     }
     public int numIslands(char[][] grid) {
-        int[][] vis = new int[grid.length][grid[0].length];
+        
         int connected = 0;
         for(int i = 0; i<grid.length; i++ ){
             for(int j = 0; j < grid[0].length; j++){
-                if(vis[i][j] != 1 && grid[i][j] == '1'){
+                if(grid[i][j] != '2' && grid[i][j] == '1'){
                     connected++;
-                    dfs(i,j,grid,vis);
+                    dfs(i,j,grid);
                 }
             }
         }
