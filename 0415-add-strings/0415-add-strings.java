eea44e076d46a8d66b2f018ee6760e0372304408
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-04-04 23:04:25 +0530

    Time: 1 ms (100%), Space: 42.9 MB (40.11%) - LeetHub

--- 0415-add-strings/0415-add-strings.java
+++ 0415-add-strings/0415-add-strings.java
@@ -1,18 +1,20 @@
 class Solution {
     public String addStrings(String num1, String num2) {
+        int i = num1.length()-1; 
+        int j = num2.length()-1;
+        int carry = 0;
         StringBuilder s = new StringBuilder();
-        int n1 = num1.length()-1;
-        int n2 = num2.length()-1;
-        int carry =0;
-        while(n1 >=0 || n2 >=0){
-            int res =( n1>=0?num1.charAt(n1)-'0': 0) + (n2 >= 0? num2.charAt(n2) - '0':0) + carry;
-            s.append(res%10);
-            carry = res/10;
-            n1--;
-            n2--;
+        while(i >=0 || j >=0){
+            int sum = (i >= 0 ?num1.charAt(i)- '0' : 0) + (j>= 0? num2.charAt(j) - '0': 0) + carry;
+            
+            i--;
+            j--;
+            s.append(sum%10);
+             carry = sum/10;
+            
         }
-        if(carry == 1 && n1 <0 && n2 < 0){
-            s.append("1");
+        if(carry > 0){
+            s.append(carry);
         }
         return s.reverse().toString();
     }
