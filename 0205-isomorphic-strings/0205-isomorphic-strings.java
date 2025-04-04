02f95dab1199fc7d7081c0e9c7a976fab6245d43
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-04-05 00:10:54 +0530

    Time: 5 ms (97.89%), Space: 42.2 MB (97.17%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,34 +1,16 @@
 class Solution {
-    public boolean isIsomorphic(String s, String t) {
-        if(s.length() != t.length()){
-            return false;
-        }
-        char[] map = new char[256];
-        int i = 0;
-        for(char ch : s.toCharArray()){
-            if(map[ch] == '\u0000'){
-                map[ch]=t.charAt(i);
-                
-            }
-            else if(map[ch] != t.charAt(i)){
-                return false;
-            }
-            i++;
-        }
-        i=0;
-        for(int j =0; j<256; j++){
-            map[j]='\u0000';
-        }
-        for(char ch : t.toCharArray()){
-            if(map[ch] == '\u0000'){
-                map[ch]=s.charAt(i);
-                
-            }
-            else if(map[ch] != s.charAt(i)){
-                return false;
-            }
-            i++;
+    boolean valid(String s, String t){
+        if(s.length() != t.length())return false;
+        char[] arr = new char[256];
+        for(int i = 0; i < s.length(); i++){
+            char c1 = s.charAt(i);
+            char c2 = t.charAt(i);
+            if(arr[c1] != '\u0000' && arr[c1] != c2 )return false;
+            arr[c1] = c2;
         }
         return true;
+    }
+    public boolean isIsomorphic(String s, String t) {
+        return valid(s,t) && valid(t,s);
     }
 }
