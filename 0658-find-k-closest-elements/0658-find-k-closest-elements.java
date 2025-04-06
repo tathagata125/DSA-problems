a15680159ed043cbecfd2f60f46907eaa4d8dfdc
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-04-06 18:31:21 +0530

    Time: 6 ms (44.41%), Space: 46 MB (48.92%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.java
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.java
@@ -1,35 +1,38 @@
 class Solution {
-    int giveFloor(int[] arr,int x){
-        int l =0;
-        int h=arr.length-1;
-        while(l <= h){
-            int mid = l + (h-l)/2;
-            if(arr[mid] >= x){
-                h=mid - 1;
+    int floorel(int[] arr, int x){
+        int l = 0;
+        int r = arr.length-1;
+        while(l <= r){
+            int mid = l + (r - l)/2;
+            if(arr[mid] < x ){
+                l = mid + 1;
             }
             else{
-                l=mid + 1;
+                r = mid - 1;
             }
         }
-        return h;
+        return r;
     }
     public List<Integer> findClosestElements(int[] arr, int k, int x) {
-        int floor = giveFloor(arr,x);
-        int ceil = floor + 1;
-        for(int i=0; i<k ; i++){
-            int floorDist = (floor == -1)? (Integer.MAX_VALUE ): x - arr[floor];
-            int ceilDist = (ceil == arr.length) ? Integer.MAX_VALUE : arr[ceil] - x;
-            if(floorDist <= ceilDist){
+        List<Integer> l = new ArrayList<>();
+        
+        int floor = floorel(arr,x) ;
+        System.out.println(floor);
+        int ceil = floor+1;
+        for(int i = 0; i < k; i++){
+            int floordest =floor < 0?Integer.MAX_VALUE: (x-arr[floor]);
+            int ceildest = ceil ==arr.length? Integer.MAX_VALUE: arr[ceil] - x;
+            if(floordest <= ceildest){
                 floor--;
             }
             else{
-                ceil ++ ;
+                ceil++;
             }
         }
-        List<Integer> res = new ArrayList<>();
-        for(int i =floor+1; i<ceil; i++){
-            res.add(arr[i]);
+        
+        for(int i = floor+1; i < ceil ; i++){
+            l.add(arr[i]);
         }
-        return res;
+        return l;
     }
 }
