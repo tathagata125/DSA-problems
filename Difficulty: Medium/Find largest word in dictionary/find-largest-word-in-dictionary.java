41dbf87847ca03ca52b73a7e63e07cc20e9d0420
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-04-06 15:48:24 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Find largest word in dictionary/find-largest-word-in-dictionary.java
+++ Difficulty: Medium/Find largest word in dictionary/find-largest-word-in-dictionary.java
@@ -17,7 +17,9 @@ class GFG {
 
             Solution ob = new Solution();
             System.out.println(ob.findLongestWord(S, d));
-        }
+        
+System.out.println("~");
+}
     }
 }
 
@@ -27,40 +29,49 @@ class GFG {
 // User function Template for Java
 
 class Solution {
-    static String findLongestWord(String S, List<String> d) {
-        int maxi = Integer.MIN_VALUE;
-        String ss="" ;
-        for(int i =0; i<d.size();i++){
+    static String findLongestWord(String s, List<String> d) {
+        int i = 0; 
+        int j = 0;
+        int k = 0;
+        String result = "";
+        while(i < d.size()){
             String str = d.get(i);
-            int j=0; 
-            int k=0;
-            while(j<S.length() && k<str.length()){
-                if(str.charAt(k) == S.charAt(j)){
+            while(j < str.length() && k < s.length()){
+                if(str.charAt(j) == s.charAt(k)){
                     j++;
                     k++;
                 }
                 else{
-                    j++;
+                    k++;
                 }
             }
-            if(k>maxi && k==str.length()){
-                ss=str;
-                maxi = str.length();
-            }
-            else if(maxi == k && k==str.length()){
-                int l =0;
-                for(;l<ss.length();l++){
-                    if(ss.charAt(l) > str.charAt(l)){
-                        ss = str;
-                        maxi = str.length();
-                        break;
-                    }
-                    else if(ss.charAt(l) < str.charAt(l)){
-                        break;
+            if(j == str.length() && result.length() > 0 ){
+                if(result.length() < str.length()){
+                    result = str;
+                }
+                else if(result.length() == str.length()){
+                    int l = 0;
+                    int r = 0;
+                    while( l < result.length() && r < str.length()){
+                        if(result.charAt(l) < str.charAt(r))break;
+                        else if(result.charAt(l) > str.charAt(r)){
+                            result = str;
+                            break;
+                        }
+                        else{
+                            l++;
+                            r++;
+                        }
                     }
                 }
             }
+            else if(j == str.length() && result.length() == 0){
+                result = str;
+            }
+            i++;
+            j=0;
+            k=0;
         }
-        return ss;
+        return result;
     }
 }
