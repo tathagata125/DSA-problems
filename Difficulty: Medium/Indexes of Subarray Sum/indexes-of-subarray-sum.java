c21ab4751aba718efdf6890201c4f282f0609093
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-04-10 16:20:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Indexes of Subarray Sum/indexes-of-subarray-sum.java
@@ -0,0 +1,56 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine().trim());
+
+        while (t-- > 0) {
+            String line = read.readLine().trim();
+            String[] numsStr = line.split(" ");
+            int[] nums = new int[numsStr.length];
+            for (int i = 0; i < numsStr.length; i++) {
+                nums[i] = Integer.parseInt(numsStr[i]);
+            }
+
+            int d = Integer.parseInt(read.readLine().trim());
+
+            Solution ob = new Solution();
+            ArrayList<Integer> result = ob.subarraySum(nums, d);
+            // Print all elements in the result list
+            for (int i : result) {
+                System.out.print(i + " ");
+            }
+            System.out.println(); // Print a new line after the result
+            System.out.println("~");
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+
+class Solution {
+    static ArrayList<Integer> subarraySum(int[] arr, int target) {
+        HashMap<Integer, Integer> m = new HashMap<>();
+        int sum = 0;
+        m.put(0,-1);
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i<arr.length;i++){
+            sum += arr[i];
+            if(m.containsKey(sum - target)){
+                res.add(m.get(sum-target) + 2);
+                res.add(i+1);
+                return res;
+            }
+            if(sum != 0)
+            m.put(sum,i);
+            
+        }
+        res.add(-1);
+        return res;
+    }
+}
