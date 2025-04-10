99e4d9c7729a628bab21d8b6d0adcbac19ba5906
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-04-10 15:04:22 +0530

    Time: 34 ms (36.82%), Space: 62.7 MB (17.31%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,30 +1,23 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
         Deque<Integer> q = new ArrayDeque<>();
-        ArrayList<Integer> arr = new ArrayList<>();
-        
+        ArrayList<Integer> res = new ArrayList<>();
+        for(int i = 0; i < nums.length; i++){
+            while(q.size() > 0 && nums[q.getLast()] < nums[i]){
+                q.removeLast();
+            }
+            q.addLast(i);
 
-        for(int i =0; i<nums.length ; i++){
-            if(q.size() != 0 && nums[q.getLast()] < nums[i]){
-                while(q.size() != 0 && nums[q.getLast()] < nums[i]){
-                    q.removeLast();
-                }
-                q.addLast(i);
-            }
-            else{
-                q.addLast(i);
-            }
-            if((i - q.getFirst() + 1) > k){
+            if((i-q.getFirst())+1 > k){
                 q.removeFirst();
             }
-           if(i >= k-1){
-            arr.add(nums[q.getFirst()]);
-           }
+            if(i >= k-1)
+            res.add(q.getFirst());
         }
-        int[] temp= new int[arr.size()];
-        for(int i =0; i<arr.size(); i++){
-            temp[i] = arr.get(i);
+        int[] arr = new int[res.size()];
+        for(int i= 0; i<res.size(); i++){
+            arr[i]=nums[res.get(i)];
         }
-        return temp;
+        return arr;
     }
 }
