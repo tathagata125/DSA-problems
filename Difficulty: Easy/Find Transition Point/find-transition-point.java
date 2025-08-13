7f8a77a681c24cea8f343ee329f00cdb3b3301a7
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-08-13 15:04:38 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Find Transition Point/find-transition-point.java
+++ Difficulty: Easy/Find Transition Point/find-transition-point.java
@@ -1,44 +1,19 @@
-//{ Driver Code Starts
-import java.io.*;
-import java.util.*;
-
-class Sorted_Array {
-    public static void main(String args[]) throws IOException {
-        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(read.readLine());
-        while (t > 0) {
-            String inputLine[] = read.readLine().trim().split(" ");
-            int n = inputLine.length;
-            int arr[] = new int[n];
-
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(inputLine[i]);
-            }
-
-            Solution obj = new Solution();
-            System.out.println(obj.transitionPoint(arr));
-            System.out.println("~");
-            t--;
-        }
-    }
-}
-// } Driver Code Ends
-
-
 class Solution {
     int transitionPoint(int arr[]) {
+        // code here
+        if(arr[arr.length-1] == 0)return -1;
         int l = 0;
-        int r = arr.length-1;
-        while(l <= r){
-            int mid = l + (r - l)/2;
-            if(arr[mid] == 1){
-                r = mid - 1;
-            }
-            else{
+        int h = arr.length - 1;
+        int mid;
+        while(l <= h){
+            mid = l + (h - l)/2;
+            if(arr[mid] == 0){
                 l = mid + 1;
             }
+            else if(arr[mid] == 1){
+                h = mid - 1;
+            }
         }
-        if( l == arr.length)return -1;
         return l;
     }
 }
