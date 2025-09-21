d6e020bb1bed412c98dc383e734164669a1b406d
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-09-21 21:01:52 +0530

    Time: 1 ms (99.96%), Space: 43.5 MB (79.62%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,22 +1,30 @@
 class Solution {
-    List<List<Integer>>result;
-    public void getSubsequence(int[] nums, int idx, List<Integer> output){
+    List<List<Integer>> result ;
+    void subset(int[] nums, int idx , List<Integer> output){
         if(idx == nums.length){
-            result.add(new ArrayList<>(output));
-            return;
+            
+        result.add(new ArrayList(output));
+                
+           
+        
+                
+            
+            return ;
         }
         output.add(nums[idx]);
-        getSubsequence(nums, idx+1, output);
+        subset(nums, idx+1,output);
         output.remove(output.size() - 1);
-        if(output.size() > 0 && output.get(output.size() - 1) == nums[idx])
-            return;
-        getSubsequence(nums, idx + 1, output);
+        if(output.size() != 0 && output.get(output.size() - 1) == nums[idx])
+            return ;
+        
+        subset(nums, idx+1,output);
+
     }
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        Arrays.sort(nums);
         result = new ArrayList<>();
-        List<Integer>output = new ArrayList<>();
-        getSubsequence(nums, 0 ,output );
+        List<Integer> l = new ArrayList<>();
+        Arrays.sort(nums);
+        subset(nums, 0,l);
         return result;
     }
 }
