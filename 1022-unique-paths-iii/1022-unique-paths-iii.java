aedcb5f2a5311e8357b97c018a03f8f816391e52
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2025-09-22 20:46:17 +0530

    Time: 0 ms (100%), Space: 40.6 MB (97.29%) - LeetHub

--- 1022-unique-paths-iii/1022-unique-paths-iii.java
+++ 1022-unique-paths-iii/1022-unique-paths-iii.java
@@ -1,46 +1,45 @@
 class Solution {
-    int startCol = -1, startRow = -1;
-    int endCol = -1, endRow = -1;
-    public int paths(int[][] grid, int row, int col, int freeCells){
-        if(col < 0 || row < 0 || col == grid[0].length || row == grid.length){
+    
+    
+    int allPaths(int[][] grid, int row, int col, int count){
+        if(col < 0 || row < 0 || col == grid[0].length || row == grid.length)return 0;
+
+        if(grid[row][col] == -1 || grid[row][col] == 3){
             return 0;
         }
-        if(grid[row][col] == -2 || grid[row][col] == -1){
+        if(grid[row][col] == 2 ){
+            if(count == 0)
+            return 1;
             return 0;
         }
-        if(row == endRow && col == endCol){
-            if(freeCells == 0)return 1;
-            else 
-                return 0;
-        }
-        grid[row][col] = -2;
-        int path = 0;
-        path +=paths(grid, row + 1, col,freeCells - 1);
-        path += paths(grid,row,col + 1,freeCells - 1);
-        path += paths(grid,row-1,col,freeCells - 1);
-        path += paths(grid,row,col-1,freeCells - 1);
-        grid[row][col] = 0;
-        return path;
         
+            grid[row][col]=3;
+        int cou =0;
+        cou +=  allPaths(grid, row-1, col,count - 1);
+        cou += allPaths(grid, row+1,col, count - 1);
+        cou += allPaths(grid, row, col-1,count - 1);
+        cou += allPaths(grid, row,col+1,count - 1);
+        
+            grid[row][col] = 0;
+        return cou;
     }
     public int uniquePathsIII(int[][] grid) {
-        int freeCells = 1;
-        for(int i =0; i<grid.length; i++){
-            for(int j=0; j<grid[0].length; j++){
-                if(grid[i][j]==0){
-                    freeCells++;
+        int row=0 ;
+        int col=0;
+        
+        int count = 1;
+        for(int i = 0; i < grid.length; i++){
+            for(int j = 0; j < grid[0].length; j++){
+                if(grid[i][j] == 1){
+                    row = i;
+                    col = j;
+                
                 }
-                else if(grid[i][j]==1){
-                    startCol=j;
-                    startRow = i;
-                }
-                else if(grid[i][j] == 2){
-                    endRow = i;
-                    endCol = j;
-                }
+                if(grid[i][j]== 0)
+                count++;
             }
-           
         }
-         return paths(grid,startRow, startCol,freeCells);
+        return allPaths(grid, row, col,count);
+        
     }
 }
