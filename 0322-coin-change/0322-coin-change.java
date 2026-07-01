b9af58250d9e31df10e0bef430a92da2ed96906e
tathagata125 <144534128+tathagata125@users.noreply.github.com>
2026-07-01 14:19:27 +0530

    Time: 24 ms (32%), Space: 46.7 MB (36.27%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,28 +1,19 @@
 class Solution {
-    int ways(int[] coins, int amount, int[] memo) { // Removed idx here
-        if (amount == 0) return 0;
-        if (amount < 0) return Integer.MAX_VALUE;
-        if (memo[amount] != 0) return memo[amount];
-
+    int ways(int[] coins, int amount, int[] memo){
+        if(amount == 0)return 0;
+        if(amount < 0)return Integer.MAX_VALUE;
+        if(memo[amount] != 0)return memo[amount];
         int min = Integer.MAX_VALUE;
-        for (int i = 0; i < coins.length; i++) {
-            // Removed passing 'i' as idx here
-            int res = ways(coins, amount - coins[i], memo); 
-            if (res != Integer.MAX_VALUE) {
-                min = Math.min(min, res);
-            }
+        for(int i = 0; i<coins.length; i++){
+                min = Math.min(min,ways(coins,amount-coins[i],memo));
         }
-
-        if (min < Integer.MAX_VALUE) {
-            return memo[amount] = min + 1;
-        }
-        return memo[amount] = Integer.MAX_VALUE;
+        if(min < Integer.MAX_VALUE)return memo[amount] = min +1;
+        return  memo[amount] = Integer.MAX_VALUE;
     }
-
     public int coinChange(int[] coins, int amount) {
-        int[] memo = new int[amount + 1];
-        int call = ways(coins, amount, memo); // Removed 0 here
-        if (call == Integer.MAX_VALUE) return -1;
+        int[] memo = new int[amount+1];
+        int call = ways(coins,amount,memo) ;
+        if(call == Integer.MAX_VALUE)return -1;
         return call;
     }
 }
